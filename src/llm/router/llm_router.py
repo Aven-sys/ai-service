7b7b4055ccefd770b5f41790b4a9c6d784c5483f
hxg-avensys <H.Xiang@aven-sys.com>
2024-11-01 15:30:51 +0800

    updated

--- src/llm/router/llm_router.py
+++ src/llm/router/llm_router.py
@@ -15,35 +15,9 @@ router = APIRouter(
 async def llm_chat(llm_chat_request_dto: LLMChatRequestDto):
     if llm_chat_request_dto.structure_output_config:
         pydantic_config = llm_chat_request_dto.structure_output_config
-        # model = pydantic_util.create_recursive_dynamic_pydantic_model(pydantic_config)
-        # Print the model schema to view structure and fields
-        # for field_name, field_info in model.model_fields.items():
-        # print(f"  Field: {field_name}")
-        # print(f"  Default: {field_info.default}")
-        # print(f"  Description: {field_info.description}")
-        # print()
-
-        # print(json.dumps(pydantic_config))
-#         config_json = """
-# [
-#     {"name": "storeName", "type": "str", "defaultValue": "BestShop", "description": "The name of the store", "nestedConfig": []},
-#     {"name": "location", "type": "str", "defaultValue": "Downtown", "description": "The store's location", "nestedConfig": []},
-#     {"name": "customers", "type": "List[Dict]", "defaultValue": [], "description": "List of customers", "nestedConfig": [
-#         {"name": "id", "type": "int", "defaultValue": 1, "description": "Customer ID", "nestedConfig": []},
-#         {"name": "name", "type": "str", "defaultValue": "John Doe", "description": "Customer name", "nestedConfig": []},
-#         {"name": "address", "type": "Dict", "defaultValue": {}, "description": "Customer address", "nestedConfig": [
-#             {"name": "street", "type": "str", "defaultValue": "123 Main St", "description": "Street address", "nestedConfig": []}
-#         ]}
-#     ]}
-# ]
-# """
         DynamicModel1 = create_pydantic_model_from_config(json.dumps(pydantic_config))
          # Print the model schema to view structure and fields
         print_model_fields(DynamicModel1)
-        # for field_name, field_info in DynamicModel1.model_fields.items():
-        #     print(f"  Field: {field_name}")
-        #     print(f"  Default: {field_info.default}")
-        #     print(f"  Description: {field_info.description}")
-        #     print()
+   
 
     return {"message": "Hello World"}
